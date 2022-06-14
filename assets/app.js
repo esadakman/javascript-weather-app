744ecab98cddf0f4314c0fc418b7906bebff65a4
esadakman <esadd26@gmail.com>
2022-06-14 14:59:38 +0300

    html and css finished

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+// https://api.openweathermap.org/data/2.5/weather?q=Ankara&units=metric&appid=ceb84ee212e3c4957be78d213c7ac491
+
+// const { default: axios } = require("axios");
+
+// let weather = {
+//   apiKey: "ceb84ee212e3c4957be78d213c7ac491",
+//   fetchWeather: function () {
+//     fetch(
+//       "https://api.openweathermap.org/data/2.5/weather?q=Ankara&units=metric&appid=ceb84ee212e3c4957be78d213c7ac491"
+//     )
+//       .then((response) => response.json())
+//       .then((data) => console.log(data));
+//   },
+// };
+
+// weather.fetchWeather();
